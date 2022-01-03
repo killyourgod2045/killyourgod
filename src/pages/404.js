d70cd95be3339661b86ddbd08ae561a6ba50aa5c
Kill Your God <95645858+killyourgod2045@users.noreply.github.com>
2022-01-03 09:44:15 -0500

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ export default function NotFound() {
         <Layout page="404" bgColor="inherit">
             <div className={notFoundStyles.notFound__container}>
                 <Link to="/">
-                    <h1>Sorry, couldn't find that page.</h1>
+                    <h1>404, REALITY NOT FOUND.</h1>
                 </Link>
             </div>
         </Layout>
